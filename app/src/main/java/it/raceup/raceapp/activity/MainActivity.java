439394014afc174e6058d5bd9d67bb170d2dc34e
stefano <sirfoga@protonmail.com>
2018-03-17 10:18:08 +0100

    fix nav bar main

--- app/src/main/java/it/raceup/raceapp/activity/MainActivity.java
+++ app/src/main/java/it/raceup/raceapp/activity/MainActivity.java
@@ -50,14 +50,13 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
-        // setSupportActionBar(toolbar);
+        setSupportActionBar(toolbar);
 
         setupFabActions();
 
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        // todo deprecated drawer.setDrawerListener(toggle);
         toggle.syncState();
 
         NavigationView navigationView = findViewById(R.id.nav_view);
